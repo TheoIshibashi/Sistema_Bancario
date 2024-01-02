f63e9b131577021af75a0f05162faa9d5c27d126
Theo Ishibashi <theoshiba2005@gmail.com>
2024-01-02 20:20:02 -0300

    Change texts

--- ProgramaBancario.py
+++ ProgramaBancario.py
@@ -1,13 +1,3 @@
-"""
-Bancario. Escreva um programa que registre o caixe de um banco. O programa
-comeca perguntando se o usuario quer criar uma conta ou fechar o programa.
-Se ele escolher fechar o programa escreva uma mensagemd e agradecimento e fecha,
-caso contrario ele vai pedir que o usuario selecione um numero com 6 digitos e,
-entao, se o numero nao existir no registro do banco, ele ira pedir um valor de
-deposito. Depois o banco perguntara se deseja ver o saldo do banco, se sim ele
-devera imprimir o balanco geral do banco, senao ele entrara em loop.
-"""
-
 contas = []
 depositos = []
 saldo = 0
